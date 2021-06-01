5fb3a3b913291ebd32dfb6859cd82b176be901fd
Damon <lolmemedon@gmail.com>
2021-06-01 17:10:51 -0400

    fixed changes

--- src/main/java/me/arynxd/button_utils/pagination/Paginator.java
+++ src/main/java/me/arynxd/button_utils/pagination/Paginator.java
@@ -4,8 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 public interface Paginator {
     // Default time for paginator to timeout due to the interaction token expiring
-    int DEFAULT_TIMEOUT = 900;
-    TimeUnit DEFAULT_TIMEOUT_UNIT = TimeUnit.SECONDS;
+    int DEFAULT_TIMEOUT = 15;
+    TimeUnit DEFAULT_TIMEOUT_UNIT = TimeUnit.MINUTES;
     boolean DEFAULT_DELETE_ON_TIMEOUT = true;
 
     void paginate();
